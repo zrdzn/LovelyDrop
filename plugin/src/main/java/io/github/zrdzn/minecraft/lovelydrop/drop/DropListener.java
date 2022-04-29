91e67d2d78425b3bdb24fa93b9a77a17b357f201
zrdzn <zrdzn9@gmail.com>
2022-04-30 00:36:06 +0200

    Secure from breaking on worldguard regions

--- plugin/src/main/java/io/github/zrdzn/minecraft/lovelydrop/drop/DropListener.java
+++ plugin/src/main/java/io/github/zrdzn/minecraft/lovelydrop/drop/DropListener.java
@@ -28,6 +28,7 @@ import org.bukkit.block.Block;
 import org.bukkit.enchantments.Enchantment;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.inventory.ItemStack;
@@ -57,7 +58,7 @@ public class DropListener implements Listener {
         this.userCache = userCache;
     }
 
-    @EventHandler
+    @EventHandler(priority = EventPriority.HIGHEST, ignoreCancelled = true)
     public void onSourceBreak(BlockBreakEvent event) {
         Player player = event.getPlayer();
 
