6bd4bd4cb9d4dd5c53f2f7de523a3edc4ff61b2f
zrdzn <zrdzn9@gmail.com>
2023-04-10 20:32:23 +0200

    GH-29 Change Gradle dsl to Kotlin

---
+++ plugin/build.gradle.kts
@@ -0,0 +1,26 @@
+import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(project(":spigot"))
+    implementation(project(":v1_8"))
+    implementation(project(":v1_12"))
+    implementation(project(":v1_13"))
+
+    compileOnly("org.spigotmc:spigot-api:1.8-R0.1-SNAPSHOT")
+
+    implementation("dev.triumphteam:triumph-gui:3.1.4")
+}
+
+java {
+    withSourcesJar()
+}
+
+tasks.withType<ShadowJar> {
+    archiveFileName.set("LovelyDrop v${project.version}.jar")
+
+    relocate("dev.triumphteam.gui", "io.github.zrdzn.minecraft.lovelydrop.gui")
+}
