064cb15266ff1026202b23449b630f2d05c08c03
zrdzn <zrdzn9@gmail.com>
2023-08-14 20:06:58 +0200

    GH-43 Fix messages not updating in drop command

--- plugin/src/main/java/io/github/zrdzn/minecraft/lovelydrop/drop/DropCommand.java
+++ plugin/src/main/java/io/github/zrdzn/minecraft/lovelydrop/drop/DropCommand.java
@@ -1,7 +1,7 @@
 package io.github.zrdzn.minecraft.lovelydrop.drop;
 
+import io.github.zrdzn.minecraft.lovelydrop.PluginConfig;
 import io.github.zrdzn.minecraft.lovelydrop.menu.MenuFacade;
-import io.github.zrdzn.minecraft.lovelydrop.message.MessageConfig;
 import io.github.zrdzn.minecraft.lovelydrop.message.MessageFacade;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
@@ -11,12 +11,12 @@ import org.jetbrains.annotations.NotNull;
 
 public class DropCommand implements CommandExecutor {
 
+    private final PluginConfig config;
     private final MessageFacade messageFacade;
-    private final MessageConfig messageConfig;
     private final MenuFacade menuFacade;
 
-    public DropCommand(MessageFacade messageFacade, MessageConfig messageConfig, MenuFacade menuFacade) {
-        this.messageConfig = messageConfig;
+    public DropCommand(PluginConfig config, MessageFacade messageFacade, MenuFacade menuFacade) {
+        this.config = config;
         this.messageFacade = messageFacade;
         this.menuFacade = menuFacade;
     }
@@ -25,14 +25,14 @@ public class DropCommand implements CommandExecutor {
     public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, String[] args) {
         // Check if sender is console.
         if (!(sender instanceof Player)) {
-            this.messageFacade.sendMessageAsync(sender, this.messageConfig.getExecutedAsConsole());
+            this.messageFacade.sendMessageAsync(sender, this.config.getMessages().getExecutedAsConsole());
             return true;
         }
 
         Player player = (Player) sender;
 
         if (!player.hasPermission("lovelydrop.menu.open")) {
-            this.messageFacade.sendMessageAsync(sender, this.messageConfig.getNoPermissions());
+            this.messageFacade.sendMessageAsync(sender, this.config.getMessages().getNoPermissions());
             return true;
         }
 
