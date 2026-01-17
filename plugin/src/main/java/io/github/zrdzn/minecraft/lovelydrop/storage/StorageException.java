f67bae9afe4baa3d7c82476f7424988fdc81aa16
zrdzn <zrdzn9@gmail.com>
2026-01-17 15:07:35 +0100

    Remove unused exception ctor

--- plugin/src/main/java/io/github/zrdzn/minecraft/lovelydrop/storage/StorageException.java
+++ plugin/src/main/java/io/github/zrdzn/minecraft/lovelydrop/storage/StorageException.java
@@ -2,10 +2,6 @@ package io.github.zrdzn.minecraft.lovelydrop.storage;
 
 public class StorageException extends RuntimeException {
 
-    public StorageException(String message) {
-        super(message);
-    }
-
     public StorageException(String message, Throwable cause) {
         super(message, cause);
     }
